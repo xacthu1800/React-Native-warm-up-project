8d9706dae57e629e968a297ef7869caa48f57313
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-10-05 14:24:50 +0700

    video 4

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textFront}>Hi my name is Bin and i'm </Text>
+      <Text style={styles.header}> i'm learning computer science
+        <Text style={styles.headerchild}>NESTED</Text>
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textFront:{
+    color: "green",
+    fontSize: 27,
+    padding: 10 ,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "pink"
+  },
+  header:{
+    padding: 20, 
+    backgroundColor: "pink",
+    textAlign: "center",
+    fontSize: 30,
+    color: "blue",
+    fontWeight: "bold"
+  },
+  headerchild:{
+    padding: 0, 
+    backgroundColor: "grey",
+    textAlign: "center",
+    fontSize: 30,
+    color: "red",
+
+  }
+  
 });
