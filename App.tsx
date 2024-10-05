171b41ce596535d8ae215b3ce45f19d03a2e3218
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-10-05 14:39:29 +0700

    use useState and Button component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState<number>(0)
+
+  const handleIncrese = () =>{
+    setCount(count+1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textFront}>Hi my name is Bin and i'm </Text>
-      <Text style={styles.header}> i'm learning computer science
-        <Text style={styles.headerchild}>NESTED</Text>
-        </Text>
-      <StatusBar style="auto" />
+      <Text style={{ 
+        fontSize: 30,
+        fontWeight: "400"
+      }}>
+        count: {count}
+      </Text>
+      <View>
+        <Button 
+        color={"red"}
+        title='increase' onPress={handleIncrese}></Button>
+      </View>
     </View>
   );
 }
@@ -20,29 +33,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  textFront:{
-    color: "green",
-    fontSize: 27,
-    padding: 10 ,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "pink"
-  },
-  header:{
-    padding: 20, 
-    backgroundColor: "pink",
-    textAlign: "center",
-    fontSize: 30,
-    color: "blue",
-    fontWeight: "bold"
-  },
-  headerchild:{
-    padding: 0, 
-    backgroundColor: "grey",
-    textAlign: "center",
-    fontSize: 30,
-    color: "red",
-
-  }
   
 });
