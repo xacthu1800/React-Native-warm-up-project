1249ff7571b3140bb8f36899115c2ba4a9b7b3b4
2151012001 - Nguyen Van Bin <2151012001bin@ou.edu.vn>
2024-10-05 15:06:31 +0700

    learn TextInput it properties

--- App.tsx
+++ App.tsx
@@ -4,13 +4,50 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [count, setCount] = useState<number>(0)
+  const [name, setName] = useState<string>("")
+  const [age, setAge] = useState<number>(0)
+
 
   const handleIncrese = () =>{
     setCount(count+1)
   }
 
+
+
   return (
     <View style={styles.container}>
+      <View>
+        
+        <Text style={{fontSize:30}}>Name : {name}</Text>
+        <TextInput 
+          multiline
+          autoCapitalize={"characters"}
+          onChangeText={(value)=>setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 35
+          }}
+        
+         />  
+      </View>
+      <View>
+        <Text style={{fontSize:30}}>Age: {age}</Text>
+        <TextInput 
+        keyboardType='number-pad'
+        multiline
+        onChangeText={(value)=>setAge(+value)}
+        style={{
+          borderColor: "green",
+          borderWidth: 1,
+          width: 200,
+          padding: 35
+        }}
+        
+         />  
+      </View>
+
       <Text style={{ 
         fontSize: 30,
         fontWeight: "400"
@@ -33,5 +70,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
   
 });
